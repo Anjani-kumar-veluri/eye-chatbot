266bc116dcf470b4a783331ff20210ebbad4bff2
Anjani-kumar-veluri <anjani1729151@gmail.com>
2026-05-04 10:26:58 +0530

    Set Python 3.11 for Render

--- downloads_model.py
+++ downloads_model.py
@@ -1,9 +1,11 @@
 import os
 import gdown
 
-DRIVE_FOLDER_URL = os.getenv("https://drive.google.com/drive/u/0/folders/1-Zg5HuGE4ooSQpFILv61chdPoG8YbP8e")
+DRIVE_FOLDER_URL = os.getenv("DRIVE_FOLDhttps://drive.google.com/drive/u/0/folders/1-Zg5HuGE4ooSQpFILv61chdPoG8YbP8eER_URL")
+
+print("DEBUG VALUE:", DRIVE_FOLDER_URL)  # 👈 for debugging
 
 if not DRIVE_FOLDER_URL:
     raise Exception("DRIVE_FOLDER_URL not found")
 
-gdown.download_folder(DRIVE_FOLDER_URL, output=".", quiet=False, use_cookies=False)
+gdown.download_folder(DRIVE_FOLDER_URL, quiet=False, use_cookies=False)
